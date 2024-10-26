8ee7dc0c58d6d0b5de69ece11df8a1508f9fa129
drishya700 <110844521+drishya700@users.noreply.github.com>
2024-10-26 08:51:01 +0530

    Chapter 3 Practice Set

---
+++ Chapter 3-PS/04_problem4.py
@@ -0,0 +1,6 @@
+name = "Drishya is a good  boy  and"
+
+print(name.replace("  "," ")) #This doesen't change the original string it just creates a new one and prints it with the edits you want
+print(name)
+
+#String are immutable which means you can't change them using string functions
