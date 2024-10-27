86c7b924b9dc0457ecb52f9068730bf817c922ec
drishya700 <110844521+drishya700@users.noreply.github.com>
2024-10-27 13:04:49 +0530

    Chapter 1 Practice Set

--- Chapter 1-PS/problem1.py
+++ Chapter 1-PS/problem1.py
@@ -1,3 +1,5 @@
+#Write a program to print Twinkle twinkle little star poem in python.
+
 print('''Twinkle Twinkle, Little Star
 How I wonder what you are
 Up above the world so high
--- Chapter 1-PS/problem4.py
+++ Chapter 1-PS/problem4.py
@@ -1,3 +1,6 @@
+# Write a python program to print the contents of a directory using the os module.
+# Search online for the function which does that
+
 import os
 
 # Specify the directory path (or use '.' for the current directory)
