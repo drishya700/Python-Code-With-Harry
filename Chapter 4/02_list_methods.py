08b3f05c61ac057c9f1dbfde1d7ed7c7539bc17f
drishya700 <110844521+drishya700@users.noreply.github.com>
2024-10-28 07:53:09 +0530

    List Methods

---
+++ Chapter 4/02_list_methods.py
@@ -0,0 +1,16 @@
+friends = ["Apple","Orange",5, 345.06, False, "Aakash","Rohan"]
+print(friends)
+
+friends.append("Harry")
+
+print(friends)
+#Lists are Mutable
+
+l1 = [1,34,62,2,6,11]
+#l1.sort()
+#l1.reverse()
+#l1.insert(3,3333333) #Insert 3333333 such that its index in the list is 3
+print(l1)
+value = l1.pop(3)
+print(value)
+print(l1)
