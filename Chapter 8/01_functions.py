322e84789f3236a19a7768f0d72ab16c56660f17
drishya700 <110844521+drishya700@users.noreply.github.com>
2024-12-28 17:12:52 +0530

    Functions in Python

---
+++ Chapter 8/01_functions.py
@@ -0,0 +1,24 @@
+# a = 12
+# b = 45
+# c = 56
+
+# average = (a+b+c)/3
+# print(average)
+
+# a = int(input("Enter Number: "))
+# b = int(input("Enter Number: "))
+# c = int(input("Enter Number: "))
+
+# average = (a+b+c)/3
+# print(average)
+
+
+def avg():
+    a = int(input("Enter Number: "))
+    b = int(input("Enter Number: "))
+    c = int(input("Enter Number: "))
+
+    average = (a+b+c)/3
+    print(average)
+
+avg()
